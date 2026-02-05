38748530927a4527f49f28edd15dc775c873cd35
unknown <Eng_Ahmed_Mostafa>
2026-02-05 13:16:15 -0800

    install tailwind & add navbar to user layout

--- src/app/components/user-nav/user-nav.ts
+++ src/app/components/user-nav/user-nav.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { BadgeModule } from 'primeng/badge';
+import { AvatarModule } from 'primeng/avatar';
+import { InputTextModule } from 'primeng/inputtext';
+import { CommonModule } from '@angular/common';
+import { Ripple } from 'primeng/ripple';
+import { Menubar } from 'primeng/menubar';
+
 
 @Component({
   selector: 'app-user-nav',
-  imports: [],
+  imports: [BadgeModule, AvatarModule, InputTextModule, Ripple, CommonModule,Menubar],
   templateUrl: './user-nav.html',
   styleUrl: './user-nav.scss',
 })
-export class UserNav {
+export class UserNav implements OnInit {
+  items: MenuItem[] | undefined;
 
+    ngOnInit() {
+        this.items = [
+            {
+                label: 'Home',
+                icon: 'pi pi-home',
+            },
+            {
+                label: 'Projects',
+                icon: 'pi pi-search',
+                badge: '3',
+                items: [
+                    {
+                        label: 'Core',
+                        icon: 'pi pi-bolt',
+                        shortcut: '⌘+S',
+                    },
+                    {
+                        label: 'Blocks',
+                        icon: 'pi pi-server',
+                        shortcut: '⌘+B',
+                    },
+                    {
+                        separator: true,
+                    },
+                    {
+                        label: 'UI Kit',
+                        icon: 'pi pi-pencil',
+                        shortcut: '⌘+U',
+                    },
+                ],
+            },
+        ];
+    }
 }
