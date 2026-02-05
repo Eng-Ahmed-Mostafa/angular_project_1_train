3e3dfaa7a9cc2e08d03bb8837a1c785ab0017a0b
unknown <Eng_Ahmed_Mostafa>
2026-02-05 10:24:02 -0800

    add navbar for auth layout

--- src/app/components/auth-nav/auth-nav.ts
+++ src/app/components/auth-nav/auth-nav.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
+import { Button } from 'primeng/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-auth-nav',
-  imports: [],
+  imports: [MenubarModule,Button,RouterLink],
   templateUrl: './auth-nav.html',
-  styleUrl: './auth-nav.scss',
+  styleUrls: ['./auth-nav.scss']
 })
-export class AuthNav {
+export class AuthNav implements OnInit  {
+  items: MenuItem[] | undefined;
 
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'login',
+        icon: 'pi pi-sign-in',
+        path: 'login'
+      },
+      {
+        label: 'register',
+        icon: 'pi pi-user-plus',
+        path: 'register'
+      },
+    ];
+  }
 }
