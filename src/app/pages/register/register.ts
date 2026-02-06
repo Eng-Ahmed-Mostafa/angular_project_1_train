27b63fd7715a831e94fe85daddde38f72f7ad1e8
unknown <Eng_Ahmed_Mostafa>
2026-02-06 09:20:07 -0800

    Create Register form

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -1,11 +1,67 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, FormsModule, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { InputTextModule } from 'primeng/inputtext';
+import { SelectModule } from 'primeng/select';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { Button } from 'primeng/button';
+import { Message } from 'primeng/message';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule, InputGroupModule, InputGroupAddonModule, InputTextModule, SelectModule, InputNumberModule,ReactiveFormsModule,Button,Message],
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
 export class Register {
+  msgs:string[] = ['2']
 
+  name!: FormControl
+  email!: FormControl
+  password!: FormControl
+  repassword!: FormControl
+
+  registerationForm!: FormGroup
+
+  constructor() {
+    this.initFormControls()
+    this.initFormGroup()
+  }
+
+  initFormControls(): void {
+    this.name = new FormControl('',[Validators.required,Validators.minLength(3),Validators.maxLength(20)])
+    this.email = new FormControl('',[Validators.required,Validators.email])
+    this.password = new FormControl('',[Validators.required,Validators.minLength(8)])
+    this.repassword = new FormControl('',[Validators.required,this.passwordMatch(this.password)])
+  }
+
+  initFormGroup(): void {
+    this.registerationForm = new FormGroup({
+      name: this.name,
+      email: this.email,
+      password: this.password,
+      repassword: this.repassword
+    })
+  }
+
+  passwordMatch(pass:AbstractControl):ValidatorFn {
+    return (rePass:AbstractControl): null | {[key:string]:boolean} => {
+      if(pass.value !== rePass.value) {
+        return {passNotMatch:true}
+      }
+      else {
+        return null
+      }
+    }
+  }
+
+  submit():void {
+    if(this.registerationForm.valid) {
+      console.log(this.registerationForm.valid)
+    }else {
+      this.registerationForm.markAllAsTouched()
+      this.registerationForm.markAllAsDirty()
+    }
+  }
 }
